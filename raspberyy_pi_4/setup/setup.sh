dd3bcda9929abde5d8ed5e2d0b457fc4eb4b7d89
Alex Degtiar <alex.degtiar@gmail.com>
2025-03-09 00:51:14 -0800

    Add files via upload

---
+++ raspberyy_pi_4/setup/setup.sh
@@ -0,0 +1,40 @@
+# These commands were run to get set up. Run via `bash setup.sh`.
+
+# Run on a Raspberry Pi 3/4/5
+# Raspbian GNU/Linux
+# Version 12 (bookworm) -- needs to be a recent version.
+
+# Stop the script if any command returns nonzero status.
+set -e
+
+# Update
+sudo apt-get update
+
+# This include a lot of shortcuts for managing the services running on the pi
+cp bash_aliases ~/.bash_aliases
+source ~/.bash_aliases
+
+# Install and configure NetworkManager for rpi static ip.
+# NM should already be install on latest Raspbian
+NetworkManager.status
+NetworkManager.cpconf
+NetworkManager.restart
+NetworkManager.status
+
+# Install and configure mosquitto MQTT broker.
+mosquitto.install
+mosquitto.cpconf
+# Set the system to bring up the mosquitto broker.
+mosquitto.enable
+mosquitto.restart
+mosquitto.status
+
+# Install and configure dnsmasq dhcp/dns server
+dnsmasq.install
+dnsmasq.cpconf
+dnsmasq.enable
+dnsmasq.restart
+dnsmasq.status
+
+# Copy /etc/hosts and /etc/hostname
+host.cpconf
