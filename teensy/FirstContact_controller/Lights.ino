aa8d05fc1a8161e96c1cf39c5af0103174d6808b
Alex Degtiar <alexdegtiar@gmail.com>
2025-04-30 08:59:47 -0700

    revert 'cl', add solid off clear before inactive (unreliable)

--- teensy/FirstContact_controller/Lights.ino
+++ teensy/FirstContact_controller/Lights.ino
@@ -87,13 +87,18 @@ bool setActiveLedState() {
 }
 
 bool setInactiveLedState() {
-  return client.publish("wled/all/api", "{\"tt\": 0, \"seg\": [{ \
+  bool result = client.publish("wled/all/api", "{\"tt\": 0, \"seg\": [{ \
+    \"id\": 0, \
+    \"fx\": 0, \
+    \"bri\": 255, \
+    \"col\": [[0,0,0], [0,0,0], [0,0,0]] \
+   }]}");
+  return result && client.publish("wled/all/api", "{\"tt\": 0, \"seg\": [{ \
     \"id\": 0, \
     \"on\": true, \
     \"bri\": 255, \
     \"col\": [[255, 255, 255], [0, 0, 0], [0, 0, 0]], \
     \"fx\": 42, \
-    \"pal\": 3, \
-    \"cl\": true \
+    \"pal\": 3 \
   }]}");
 }
